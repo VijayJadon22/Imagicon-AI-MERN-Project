dbba88af0ce6222ab937411fdcb458d60cf4eb46
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-09 23:28:28 +0530

    Implemnted assests.js file for importing all the images and exporting them for ease of use

---
+++ client/src/assets/assets.js
@@ -0,0 +1,10 @@
+import imagiconLogo from "./draw.png";
+import creditStar from "./credit_star.svg";
+import profileIcon from "./profile_icon.png";
+
+
+export const assets = {
+    imagiconLogo,
+    creditStar,
+    profileIcon
+}
