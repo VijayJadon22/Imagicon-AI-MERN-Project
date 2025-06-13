92985fc55c312ddd405101db72e812a46d19d700
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-13 22:00:04 +0530

    Added animation by using Framer motion library

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { stepsData } from "../assets/assets.js";
+import { motion } from "framer-motion";
 
 const Steps = () => {
   return (
-    <div className="flex flex-col items-center mb-20">
+    <motion.div
+      className="flex flex-col items-center mb-20"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
       <h1 className="text-4xl mb-2">How it works</h1>
       <p className="text-sm text-gray-500 mb-8">
         Transform words into stunning images
       </p>
       <div className="space-y-4 flex flex-col gap-3 items-center ">
-              {stepsData.map((item, index) => (
-                  <div className="flex items-center gap-4 bg-white/60 shadow-2xl rounded-xl p-4 w-full hover:scale-[1.02] transition-all duration-300" key={index}>
-                      <img src={item.icon} alt={item.title} width={40}/>
-                      <div className="flex flex-col items-start">
-                          <p className="text-sm">{ item.title}</p>
-                          <p className="text-xs text-gray-500">{ item.description}</p>
-                      </div>
-                  </div>
-              ))}
+        {stepsData.map((item, index) => (
+          <div
+            className="flex items-center gap-4 bg-white/60 shadow-2xl rounded-xl p-4 w-full hover:scale-[1.02] transition-all duration-300"
+            key={index}
+          >
+            <img src={item.icon} alt={item.title} width={40} />
+            <div className="flex flex-col items-start">
+              <p className="text-sm">{item.title}</p>
+              <p className="text-xs text-gray-500">{item.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
