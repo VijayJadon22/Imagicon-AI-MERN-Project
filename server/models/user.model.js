b3e62d78b312bf344ecad5d0befc810e25c4f57e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-14 18:20:45 +0530

    Implemnted method to generate token for user when user login or signup

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -23,5 +25,18 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password") || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.methods.generateToken = function (userId) {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+}
+
 const User = mongoose.model("User", userSchema);
 export default User;
+
