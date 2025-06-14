851c7275da48448d9208081ce29918c06e5a3f7d
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-14 21:23:52 +0530

    Implemnted login controller function to allow user to login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -27,9 +27,28 @@ export const signup = async (req, res) => {
 }
 export const login = async (req, res) => {
     try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "All fields are required", success: false });
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "User does not exist", success: false });
+        
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "Invalid credentials", success: false });
+        }
+
+        generateTokenAndSetCookie(user, res);
+
+        return res.status(200).json({
+            success: true,
+            user: { name: user.name, email: user.email },
+            message: "User logged in successfully"
+        });
 
     } catch (error) {
-
+        console.error("Login Error:", error.message);
+        return res.status(500).json({ message: "Internal Server Error", success: false });
     }
 }
 export const logout = async (req, res) => {
