6c01ca4606208e1e35f8367a6ebcf650435ca1fa
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-14 19:36:35 +0530

    Implemnted signup controller function which lets user sign up

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js";
+import { generateTokenAndSetCookie } from "../utils/auth.js";
+
+
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required", success: false });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "User already exists", success: false });
+
+        const user = await User.create({ name, email, password });
+        generateTokenAndSetCookie(user, res);
+
+        return res.status(201).json({
+            success: true,
+            user: { name: user.name, email: user.email },
+            message: "User registered successfully"
+        });
+    } catch (error) {
+        console.error("Signup Error:", error.message);
+        return res.status(500).json({ message: "Internal Server Error", success: false });
+    }
+}
+export const login = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+export const logout = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
