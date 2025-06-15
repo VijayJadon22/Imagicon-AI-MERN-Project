434e07fe520bb75bcb2c37901f208733053d395e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-15 21:27:02 +0530

    Added get User and logout suer routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup, userCredits } from "../controllers/auth.controller.js";
+import { getUser, login, logout, signup, userCredits } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -7,7 +7,9 @@ const router = express.Router();
 router.post('/login', login);
 router.post('/signup', signup);
 
+router.get('/user', protectRoute, getUser);
 router.get('/credits', protectRoute, userCredits);
+router.get('/logout', protectRoute, logout);
 
 
 export default router;
