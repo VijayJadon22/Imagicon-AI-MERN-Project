41e6b9756a12d6928a7cd85553baf8e4d131b0d7
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-17 20:54:54 +0530

    Implemnted verifyRazorpayPayment which will used to verify the payemnt which was initated to process it and add the credits to the user document

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -21,4 +21,40 @@ export const createRazorpayOrder = async (req, res) => {
     }
 }
 
+export const verifyRazorpayPayment = async (req, res) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature, userId, credits } = req.body;
+
+        const generatedSignature = crypto
+            .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+            .update(razorpay_order_id + "|" + razorpay_payment_id)
+            .digest("hex");
+
+        if (generatedSignature === razorpay_signature) {
+
+            const user = await User.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({ success: false, message: "User not found" });
+            }
+
+            user.creditBalance += credits;
+            await user.save();
+
+            return res.status(200).json({
+                success: true,
+                message: "Payment verified & credits added successfully",
+                updatedCredits: user.creditBalance,
+            });
+
+        } else {
+            return res.status(400).json({ error: "Payment verification failed" });
+        }
+
+    } catch (error) {
+        console.error("Error verifying Razorpay payment:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
 
