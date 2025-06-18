3b74cd0fe3f63f03bde6f8de7eed44f5118599c3
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-18 23:13:43 +0530

    Imported the env variables into the razorpay instance file to get the razor secret key

--- server/utils/razorpay.js
+++ server/utils/razorpay.js
@@ -1,7 +1,7 @@
 import Razorpay from "razorpay";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
 export const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
