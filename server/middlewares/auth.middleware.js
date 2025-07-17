b6f043e0ac9f075fc30505370a705251da1aa225
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-17 20:36:58 +0530

    Made changes found user using MongoDB query findById on User model

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ export const protectRoute = async (req, res, next) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await User.findById(decoded.userId).select(-"password");
+        const user = await User.findById(decoded.userId).select("-password");
 
         if (!user) return res.status(401).json({ message: "User not found" });
 
