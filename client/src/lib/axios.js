884f4495c53cb5b471844e0afa3147e33cb86d80
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-17 22:27:05 +0530

    Made change in axios instance

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true, // Enabling the sending of credentials (such as cookies) with cross-origin requests
 })
 
