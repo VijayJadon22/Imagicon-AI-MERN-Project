79e4e893a40614bf93f8fdeac6ff7f7bddab1d18
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-19 17:33:40 +0530

    made chnages on render and in auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ export const generateTokenAndSetCookie = (user, res) => {
     const options = {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Ensures cookie security in production
-        sameSite: "None",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     }
     res.cookie("token", token, options);
